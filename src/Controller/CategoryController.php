21deb0f0a682f97477f849ed3c8a61d0642b0b7e
Yoann Coualan <yoann.coualan@gmail.com>
2023-05-03 10:13:10 +0200

    JWT

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
 class CategoryController extends AbstractController
 {
     #[Route('/categories', name: 'app_category', methods:['GET'])]
@@ -36,24 +39,48 @@ class CategoryController extends AbstractController
     #[Route('/category', name:'category_add', methods:['POST'])]
     public function add(Request $r, EntityManagerInterface $em, ValidatorInterface $v) : Response
     {
-        $category = new Category();
-        $category->setName($r->get('name')); // Récupère le paramètre 'name' de la requête et l'assigne à l'objet
+        // On récupère les infos envoyées en header
+        $headers = $r->headers->all();
+        // Si la clé 'token' existe et qu'elle n'est pas vide dans le header
+        if(isset($headers['token']) && !empty($headers['token'])){
+            $jwt = current($headers['token']); // Récupère la cellule 0 avec current()
+            $key = $this->getParameter('jwt_secret');
 
-        $errors = $v->validate($category); // Vérifie que l'objet soit conforme avec les validations (assert)
-        if(count($errors) > 0){
-            // S'il y a au moins une erreur
-            $e_list = [];
-            foreach($errors as $e){ // On parcours toutes les erreurs
-                $e_list[] = $e->getMessage(); // On ajoute leur message dans le tableau de messages
+            // On essaie de décoder le jwt
+            try{
+                $decoded = JWT::decode($jwt, new Key($key, 'HS256'));
+            }
+            // Si la signature n'est pas vérifiée ou que la date d'expiration est passée, il entrera dans le catch
+            catch(\Exception $e){
+                return new JsonResponse($e->getMessage(), 403);
             }
 
-            return new JsonResponse($e_list, 400); // On retourne le tableau de messages
+            // On regarde si la clé 'roles' existe et si l'utilisateur possède le bon rôle
+            if($decoded->roles != null  && $decoded->roles == 'ROLE_ADMIN'){
+
+                $category = new Category();
+                $category->setName($r->get('name')); // Récupère le paramètre 'name' de la requête et l'assigne à l'objet
+
+                $errors = $v->validate($category); // Vérifie que l'objet soit conforme avec les validations (assert)
+                if(count($errors) > 0){
+                    // S'il y a au moins une erreur
+                    $e_list = [];
+                    foreach($errors as $e){ // On parcours toutes les erreurs
+                        $e_list[] = $e->getMessage(); // On ajoute leur message dans le tableau de messages
+                    }
+
+                    return new JsonResponse($e_list, 400); // On retourne le tableau de messages
+                }
+
+                $em->persist($category);
+                $em->flush();
+
+                return new JsonResponse('success', 201);
+
+            }
         }
 
-        $em->persist($category);
-        $em->flush();
-
-        return new JsonResponse('success', 201);
+        return new JsonResponse('Access denied', 403);
     }
 
     #[Route('/category/{id}', name:'category_update', methods:['PATCH'])]
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+use Firebase\JWT\JWT;
+
 class UserController extends AbstractController
 {
     #[Route('/login', name: 'login', methods:['POST'])]
@@ -31,6 +33,15 @@ class UserController extends AbstractController
             return new JsonResponse('Identifiants invalides', 400);
         }
 
-        return new JsonResponse('Email et mdp ok', 200);
+        $key = $this->getParameter('jwt_secret');
+        $payload = [
+            'iat' => time(), // Issued at (date de création)
+            'exp' => time() + 3600, // Expiration (date de création + x secondes)
+            'roles' => $user->getRoles()
+        ];
+
+        $jwt = JWT::encode($payload, $key, 'HS256');
+
+        return new JsonResponse($jwt, 200);
     }
 }
